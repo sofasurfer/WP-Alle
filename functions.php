8b597e86393908deed1c10c514cfabfa301445e0
sofasurfer <kilian@sofasurfer.ch>
2019-06-07 16:42:40 +0200

    Add submission name to list in admin

--- functions.php
+++ functions.php
@@ -13,10 +13,24 @@ function remove_theme_post_format(){
 }
 
 
+function c_set_submission_colums($column, $post_id){
+
+    if( $column == 'submission' ){
+        $p_meta = get_post_meta( $post_id );
+        echo $p_meta['wpcf7s_posted-your-name'][0] . ' (' . $p_meta['wpcf7s_posted-your-email'][0] . ')';
+    }
+    error_log("cols: " . print_r($post_id,true));
+    return $atts;
+}
+
+add_action('manage_wpcf7s_posts_custom_column', 'c_set_submission_colums', 20, 2);
 
 
-
-
+function writy_setup() {
+    add_theme_support( 'align-wide' );
+    add_theme_support( 'wp-block-styles' );
+}
+add_action( 'after_setup_theme', 'writy_setup' );
 
 
 /*
@@ -39,13 +53,14 @@ function list_subscriptions( $atts ){
         $p_meta = get_post_meta( $post->ID );
         $p_meta['post_date'] = get_the_date('D j M',$post);
         $p_meta['post_name'] = $p_meta['wpcf7s_posted-your-name'][0];
+        $p_meta['post_age'] = $p_meta['wpcf7s_posted-your-age'][0];
 
         if( array_key_exists('wpcf7s_posted-checkbox-anonymous', $p_meta) ) {
             $p_meta['post_name'] = preg_replace("/[\S]/", "X", $p_meta['post_name']);
         }
 
         $sub_list .= "<tr><td>".$p_meta['post_date']."</td>";
-        $sub_list .= "<td>".$p_meta['post_name']."</td></tr>";
+        $sub_list .= "<td>".$p_meta['post_name']." (".$p_meta['post_age'].")</td></tr>";
         array_push($subscribers, $p_meta);
 
     }
@@ -58,6 +73,21 @@ function list_subscriptions( $atts ){
 add_shortcode( 'list_subscriptions', 'list_subscriptions' );
 
 
+/*
+    Count subscriptions
+*/
+function count_subscriptions( $atts ){
+    $args = array(
+      'numberposts' => -1,
+      'post_type'   => 'wpcf7s'
+    );
 
+    $get_posts = new WP_Query;
+    $posts = $get_posts->query( $args );
+
+    return "<div class=\"c-counter\">" . sprintf($atts['text'], count($posts) ) . "</div>";
+}
+
+add_shortcode( 'count_subscriptions', 'count_subscriptions' );
 
 ?>
