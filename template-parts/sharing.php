a01e5ff9fa448a021dc57f7f63f7619b387afc42
sofasurfer <kilian@sofasurfer.ch>
2020-05-29 15:36:20 +0200

    Fix footer links

--- template-parts/sharing.php
+++ template-parts/sharing.php
@@ -7,5 +7,5 @@ $vtitle = get_the_title();
     <a class="v-share-fb" href="https://www.facebook.com/sharer/sharer.php?u=<?= $vurl; ?>" target="_blank">Facebook</a>
     <a class="v-share-tw" href="https://twitter.com/intent/tweet?text=<?= $vtitle; ?>: <?= $vurl; ?>" target="_blank">Twitter</a>
     <a class="v-share-wa" href="whatsapp://send?<?= $vtitle; ?>: <?= $vurl; ?>" data-action="share/whatsapp/share">Whatsapp</a>
-    <a class="v-share-em" href="mailto:" data-action="share/whatsapp/share">E-Mail</a>
+    <a class="v-share-em" href="mailto:?subject=<?= $vtitle; ?>&body=<?= $vurl; ?>" data-action="share/whatsapp/share">E-Mail</a>
 </div>
